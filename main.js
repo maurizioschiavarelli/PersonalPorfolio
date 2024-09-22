229f577366fd74195e853e53c04730cc4e86d56d
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-09-22 18:49:12 +0200

    inizio creazione contattami

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
-/* ================== MAIN SEZIONE INCREMENTO NUMERI ================== */
+/* ================== NAVBAR ================== */
+
+let navbar = document.getElementById('nav');
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY > 10){
+        navbar.classList.add('navScroll');
+    } else {
+        navbar.classList.remove('navScroll');
+    }
+})
+
+/* ================== SECTION NUMBER ================== */
+
+//Calcolo anni di esperienza:
+let numberExp = document.getElementById('numberExp');
+
+function calcolaDifferenzaAnni(annoSelezionato) {
+    const annoCorrente = new Date().getFullYear();
+    return annoCorrente - annoSelezionato;
+}
+
+numberExp.innerHTML = calcolaDifferenzaAnni('2023');
+
+
+
 
 
