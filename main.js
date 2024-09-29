103a95c7bfa9b6242d365ad8cff46e52c22bf442
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-09-29 17:13:36 +0200

    sistemata navbar

--- main.js
+++ main.js
@@ -22,6 +22,33 @@ checkbox.addEventListener('click',()=>{
     }
 })
 
+// let body = document.querySelector('body');
+// let checkbox = document.getElementById('checkbox');
+
+// // Funzione per applicare il tema in base alle preferenze dell'utente
+// function applyTheme() {
+//     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//         body.classList.add('darkMode');
+//     } else {
+//         body.classList.add('lightMode');
+//     }
+// }
+
+// // Chiamare la funzione al caricamento della pagina
+// applyTheme();
+
+// // Aggiungere un listener per il cambiamento delle preferenze del tema
+// window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', applyTheme);
+
+// // Listener per il checkbox per cambiare manualmente il tema
+// checkbox.addEventListener('click', () => {
+//     if (body.classList.contains('lightMode')) {
+//         body.classList.replace('lightMode', 'darkMode');
+//     } else {
+//         body.classList.replace('darkMode', 'lightMode');
+//     }
+// });
+
 /* ================== SECTION NUMBER ================== */
 
 //Calcolo anni di esperienza:
