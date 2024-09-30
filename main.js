fd00170fd61f94167e07578e0e2e5276cea85202
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-09-30 22:14:41 +0200

    inizio creazione modale fine versione sito 0.3

--- main.js
+++ main.js
@@ -22,32 +22,6 @@ checkbox.addEventListener('click',()=>{
     }
 })
 
-// let body = document.querySelector('body');
-// let checkbox = document.getElementById('checkbox');
-
-// // Funzione per applicare il tema in base alle preferenze dell'utente
-// function applyTheme() {
-//     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//         body.classList.add('darkMode');
-//     } else {
-//         body.classList.add('lightMode');
-//     }
-// }
-
-// // Chiamare la funzione al caricamento della pagina
-// applyTheme();
-
-// // Aggiungere un listener per il cambiamento delle preferenze del tema
-// window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', applyTheme);
-
-// // Listener per il checkbox per cambiare manualmente il tema
-// checkbox.addEventListener('click', () => {
-//     if (body.classList.contains('lightMode')) {
-//         body.classList.replace('lightMode', 'darkMode');
-//     } else {
-//         body.classList.replace('darkMode', 'lightMode');
-//     }
-// });
 
 /* ================== SECTION NUMBER ================== */
 
@@ -60,6 +34,8 @@ function calcolaDifferenzaAnni(annoSelezionato) {
 }
 
 numberExp.innerHTML = calcolaDifferenzaAnni('2023');
+
+
 
 
 
