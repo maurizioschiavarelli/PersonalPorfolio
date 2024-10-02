c931d94dba809c4f4909bb40c278d970326e7126
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-10-02 20:16:15 +0200

    creazione modal fallout e prova smartphone

--- main.js
+++ main.js
@@ -38,26 +38,62 @@ numberExp.innerHTML = calcolaDifferenzaAnni('2023');
 /* ================== MODAL ================== */
 
 let modal1 = document.getElementById("modal1");
+let modal2 = document.getElementById("modal2");
+
 let modal1Img = document.getElementById("modal1Img");
-let exitModal = document.getElementById("exitModal");
+let modal2Img = document.getElementById("modal2Img");
 
-modal1Img.addEventListener("click", ()=>{
-    if(modal1.style.display == "none"){
-        modal1.style.display = "block";
-        modal1.classList.add("modal1block");
-        modal1.classList.add("modalShadow");
-        checkbox.disabled = true;
-    }else{
-        modal1.style.display = "none";
-    }
-})
+let exitModal = document.querySelectorAll("#exitModal");
 
-exitModal.addEventListener("click", ()=>{
-    if(modal1.style.display == "block"){
-        modal1.style.display = "none";
-        checkbox.disabled = false;
-    }
-})
+let links = document.querySelectorAll('a');
+
+console.log(exitModal);
+
+function getModal(modal, modalImg) {
+    modalImg.addEventListener("click", () => {
+        if (modal.style.display == "none") {
+            modal.classList.add("modal1");
+            modal.style.display = "block";
+            modal.classList.add("modalShadow");
+            checkbox.disabled = true;
+        } else {
+            modal.style.display = "none";
+        }
+    });
+
+    exitModal.forEach(exit => {
+        exit.addEventListener("click", () => {
+            if (modal.style.display == "block") {
+                modal.style.display = "none";
+                checkbox.disabled = false;
+            }
+        });
+    });
+
+}
+
+getModal(modal1, modal1Img);
+getModal(modal2, modal2Img);
+
+
+
+// modal1Img.addEventListener("click", ()=>{
+//     if(modal1.style.display == "none"){
+//         modal1.style.display = "block";
+//         modal1.classList.add("modal1block");
+//         modal1.classList.add("modalShadow");
+//         checkbox.disabled = true;
+//     }else{
+//         modal1.style.display = "none";
+//     }
+// })
+
+// exitModal.addEventListener("click", ()=>{
+//     if(modal1.style.display == "block"){
+//         modal1.style.display = "none";
+//         checkbox.disabled = false;
+//     }
+// })
 
 
 
